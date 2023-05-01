20aafa7ced9b50a5dfd9dc1b4bd6c95a2675d5bd
mat-jar <mtu07@poczta.fm>
2023-05-01 23:10:38 +0200

    Add solutions to tasks

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,17 @@
 class CountElements
 
+  def perform
+    count_elements
+  end
+
+  private
+
+  def initialize(array)
+    @array = array
+  end
+
+  def count_elements
+    @array.tally
+  end
+
 end
--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,18 @@
 class ValidatePassword
 
+  def perform
+    validate_password
+  end
+
+  private
+
+  def initialize(password)
+    @password = password
+  end
+
+  def validate_password
+    /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%&*+=:;?<>])\A.{6,24}\z/.match?(@password) &&
+    !/(.)\1{2}/.match?(@password)
+  end
+
 end
